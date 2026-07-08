b209b68abdc57bcbd710ed443b1f96c0c8a8f7ab
vinodkumar95836 <112252286+vinodkumar95836@users.noreply.github.com>
2026-07-08 11:21:19 +0530

    save

--- Day 1/230 Kth Smallest Element in a BST.py
+++ Day 1/230 Kth Smallest Element in a BST.py
@@ -1,10 +1,10 @@
 from typing import Optional
 
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val=0):
         self.val = val
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
@@ -22,3 +22,27 @@ class Solution:
                 return root.val
 
             root = root.right
+
+
+# Driver Code
+
+# Creating the BST:
+#         5
+#       /   \
+#      3     6
+#     / \
+#    2   4
+#   /
+#  1
+
+root = TreeNode(5)
+root.left = TreeNode(3)
+root.right = TreeNode(6)
+root.left.left = TreeNode(2)
+root.left.right = TreeNode(4)
+root.left.left.left = TreeNode(1)
+
+k = 3
+
+obj = Solution()
+print("The", k, "smallest element is:", obj.kthSmallest(root, k))
